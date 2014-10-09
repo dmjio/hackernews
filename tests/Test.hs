66646371c66709bae0ea3638f157b29c8d371e8b
David Johnson <djohnson.m@gmail.com>
2014-10-09 12:01:19 -0500

    completed tests

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,14 +1,35 @@
 module Main where
 
-import Test.Hspec
-import Web.HackerNews
-import Data.Maybe (isJust)
+import           Data.Maybe     (isJust)
+import           Test.Hspec     (it, runIO, hspec, describe)
+import           Web.HackerNews
 
 main :: IO ()
 main = hspec $ do
   describe "Hacker News API Tests" $ do
     story <- runIO $ getStory (StoryId 8863)
-    runIO $ print story
     it "Retrieves a Story" $ isJust story
-         
-  
+    comment <- runIO $ getComment (CommentId 2921983)
+    it "Retrieves a Comment" $ isJust comment
+    user <- runIO $ getUser (UserId "dmjio")
+    it "Retrieves a User" $ isJust user
+    poll <- runIO $ getPoll (PollId 126809)
+    it "Retrieves a Poll" $ isJust poll
+    pollOpt <- runIO $ getPollOpt (PollOptId 160705)
+    it "Retrieves a Pollopt" $ isJust pollOpt
+    topStories <- runIO getTopStories
+    it "Retrieves Top Stories" $ isJust topStories
+    maxItem <- runIO getMaxItem
+    it "Retrieves Max Item" $ isJust maxItem
+    updates <- runIO getUpdates
+    it "Retrieves Updates" $ isJust updates
+
+
+
+
+
+
+
+
+
+
