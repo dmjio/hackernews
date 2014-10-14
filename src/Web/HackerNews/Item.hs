cecf466cbe38431c3553d31af36281a28d6cf50f
Konstantin Zudov <konstantin@anche.no>
2014-10-15 00:29:19 +0300

    instance Endpoint ItemId Item

--- src/Web/HackerNews/Item.hs
+++ src/Web/HackerNews/Item.hs
@@ -1,14 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Web.HackerNews.Item where
 
 import           Control.Monad       (MonadPlus (mzero))
 import           Data.Aeson          (FromJSON (parseJSON), Value (Object), (.:))
 import           Data.Text           (Text)
+import           Data.Monoid         ((<>))
 import           Control.Applicative
 import           Web.HackerNews.Comment (Comment)
 import           Web.HackerNews.Poll (Poll, PollOpt)
 import           Web.HackerNews.Story (Story)
 import           Web.HackerNews.Job   (Job)
+import           Web.HackerNews.Endpoint (Endpoint(..))
+import           Web.HackerNews.Util (toText)
 
 newtype ItemId = ItemId Int deriving (Show,Eq)
 
@@ -18,6 +22,9 @@ data Item = ItemComment Comment
           | ItemStory Story
           | ItemJob Job
           deriving (Show)
+
+instance Endpoint ItemId Item where
+    endpoint (ItemId itemId) = "item/" <> toText itemId
 
 instance FromJSON Item where
     parseJSON v@(Object o) = do
