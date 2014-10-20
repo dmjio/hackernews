23083c30892b5f300e404c62d61a6956a9cb896e
David Johnson <djohnson.m@gmail.com>
2014-10-20 00:57:06 -0500

    updated examples

--- examples/Example.hs
+++ examples/Example.hs
@@ -1,12 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Example where
 
-import           Control.Monad  (liftM3)
+import           Control.Monad  (liftM2)
 import           Web.HackerNews (UserId (..), getUser, hackerNews)
 
 main :: IO ()
-main = print =<< hackerNews (liftM3 (,,) one two three)
+main = print =<< hackerNews (liftM2 (,) one two)
   where
     one   = getUser (UserId "dmjio")
     two   = getUser (UserId "dmj")
-    three = getUser (UserId "abs")
+
 
