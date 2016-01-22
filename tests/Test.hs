db44c79f504994016f32534fb93e21ea3b9ba088
David Johnson <djohnson.m@gmail.com>
2016-01-22 13:09:14 -0600

    Removed poll test

--- tests/Test.hs
+++ tests/Test.hs
@@ -10,18 +10,16 @@ main :: IO ()
 main = hspec $ do
   describe "Hacker News API Tests" $ do
     it "Retrieves all" $ do
-      result <- hackerNews $ (,,,,,,,,) <$> 
+      result <- hackerNews $ (,,,,,,,)  <$> 
         getStory   (StoryId 8863)       <*>
         getComment (CommentId 2921983)  <*>
         getUser    (UserId "dmjio")     <*>
         getJob     (JobId 8437631)      <*>
-        getPoll    (PollId 126809)      <*>
         getPollOpt (PollOptId 160705)   <*>
         getTopStories                   <*>
         getMaxItem                      <*>
         getUpdates
       result `shouldSatisfy` isRight
-  
 
 
 
