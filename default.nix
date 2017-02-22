b02651475bed49e321c2c12c7543e5a89851ca3b
David Johnson <djohnson.m@gmail.com>
2017-02-22 00:45:56 -0800

    Allow broken ghcjs and updated permissions on phantomjs build

--- default.nix
+++ default.nix
@@ -1,4 +1,8 @@
-{ pkgs ? import <nixpkgs> {}, compiler ? "ghc801" }:
+{ compiler ? "ghc801" }:
+let
+  config = { allowBroken = true; };
+  pkgs = import <nixpkgs> { inherit config; };
+in
 {
    hackernews = pkgs.haskell.packages.${compiler}.callPackage ./hackernews.nix { inherit compiler pkgs; };
 }
